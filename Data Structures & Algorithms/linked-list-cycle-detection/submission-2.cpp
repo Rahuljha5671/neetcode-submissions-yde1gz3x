bc1e740eed4c250dde326b375013f7f65bb645ee
Rahul Ranjan Jha <84356368+Rahuljha5671@users.noreply.github.com>
2026-04-16 09:27:13 -0700

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+
+        // unordered_map<ListNode*,bool>visited;
+
+        // ListNode* temp=head;
+        // bool hasCycle=false;
+
+        // while(temp!=NULL){
+        //     if(visited[temp]){
+        //         hasCycle=true;
+        //         break;
+        //     }
+        //     visited[temp]=true;
+        //     temp=temp->next;
+
+        // }
+        // return hasCycle;
+
+        ListNode* slow=head;
+        ListNode* fast=head;
+
+        bool hasCycle=false;
+
+        while(slow!=NULL && fast!=NULL){
+           
+            slow=slow->next;
+            fast=fast->next;
+            if(fast!=NULL){
+                fast=fast->next;
+            }
+            if(slow==fast && slow!=NULL){
+                hasCycle=true;
+                break;
+            }
+        }
+
+        return hasCycle;
+
+        
+    }
+};
