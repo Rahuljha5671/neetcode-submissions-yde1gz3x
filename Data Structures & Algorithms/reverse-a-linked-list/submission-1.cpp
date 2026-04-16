f3ca657949eb83ba99c69156d764ed8fa80b3eff
Rahul Ranjan Jha <84356368+Rahuljha5671@users.noreply.github.com>
2026-04-16 09:00:40 -0700

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+
+    // ListNode* insertAtHead(ListNode* newHead,ListNode * nodeToInsert){
+
+    //     if(newHead==NULL){
+    //         newHead=new ListNode(nodeToInsert->val); 
+    //     }
+    //     else{
+    //         ListNode * tempNode=new ListNode(nodeToInsert->val);
+    //         tempNode->next=newHead;
+    //         newHead=tempNode;
+    //     }
+
+    //     return newHead;
+    // }
+public:
+    ListNode* reverseList(ListNode* head) {
+
+        // if(head==NULL){
+        //     return head;
+        // }
+
+        // ListNode* temp=new ListNode();
+
+        // ListNode* newHead=NULL;
+        // while(head!=NULL){
+        //     newHead=insertAtHead(newHead,head);
+        //     head=head->next;
+        // }
+
+        // return newHead;
+
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* forward=curr;
+
+        while(curr!=NULL){
+            forward=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=forward;
+        }
+       
+
+
+        return prev;
+    }
+};
