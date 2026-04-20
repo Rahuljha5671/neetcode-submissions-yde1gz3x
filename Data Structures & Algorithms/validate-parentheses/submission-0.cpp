78489d5ffa6f92d7ca85e82fb5877df937080632
Rahul Ranjan Jha <84356368+Rahuljha5671@users.noreply.github.com>
2026-04-20 08:20:32 -0700

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        bool ans=true;
+
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='[' || s[i]=='(' || s[i]=='{'){
+                st.push(s[i]);
+            }
+            else if(s[i]==')'){
+                if(!st.empty() && st.top()=='('){
+                    st.pop();
+                }
+                else{
+                    ans=false;
+                    return ans;
+                    break;
+                }
+            }
+            else if(s[i]==']'){
+                if(!st.empty() && st.top()=='['){
+                    st.pop();
+                }
+                else{
+                    ans=false;
+                    return ans;
+                    break;
+                }
+                
+            }
+            else if(s[i]=='}'){
+                if(!st.empty() && st.top()=='{'){
+                    st.pop();
+                }
+                else{
+                    ans=false;
+                    return ans;
+                    break;
+                } 
+            } 
+        }
+        
+        if(st.empty()){
+            ans=true;
+        }
+        else{
+            ans=false;
+        }
+        return ans;
+
+ 
+    }
+};
