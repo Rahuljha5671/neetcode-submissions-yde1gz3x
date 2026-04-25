c0fe5e848291a498aea9aaba36edc30383aa2d0a
Rahul Ranjan Jha <84356368+Rahuljha5671@users.noreply.github.com>
2026-04-25 16:37:19 -0700

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.java
@@ -0,0 +1,91 @@
+class LRUCache {
+
+    int capacity;
+    Map<Integer,Node>mp=new HashMap<>();;
+
+    class Node{
+        int key;
+        int val;
+        Node next;
+        Node prev;
+
+        public Node(int key,int val){
+            this.key=key;
+            this.val=val;
+        }
+    }
+
+    Node head=new Node(-1,-1);
+    Node tail=new Node(-1,-1);
+
+    public void addNode(Node n){
+        Node temp=head.next;
+        head.next=n;
+        n.prev=head;
+        n.next=temp;
+        temp.prev=n;
+    }
+    public void deleteNode(Node n){
+        Node temp_prev=n.prev;
+        Node temp_next=n.next;
+        temp_prev.next=temp_next;
+        temp_next.prev=temp_prev;
+    }
+
+
+    public LRUCache(int capacity) {
+        this.capacity=capacity;
+        head.next=tail;
+        tail.prev=head;
+    }
+    
+    //just move the called node in bgl of head
+    public int get(int key) {
+        if(mp.containsKey(key)){
+            Node n=mp.get(key);
+            int res=n.val;
+            //delete from map
+            // mp.remove(key);
+            //delete
+            deleteNode(n);
+            //add
+            addNode(n);
+            //add in map again
+            // mp.put(key,head.next);
+            return res;
+        }
+
+        return -1;
+
+        
+    }
+    
+    //1.if already contains key then remove current and add new node in bgl of head
+    //2.if capacity is full then remove the 
+    //1.just add key
+    public void put(int key, int value) {
+        if(mp.containsKey(key)){
+            Node n=mp.get(key);
+            //delete from map
+            mp.remove(key);
+            //delete from dll
+            deleteNode(n);
+            //rest steps in common
+            addNode(new Node(key,value));
+            mp.put(key,head.next);
+        }
+        else if(mp.size()==capacity){
+            Node n=tail.prev;
+            deleteNode(n);
+            mp.remove(n.key);
+            addNode(new Node(key,value));
+            mp.put(key,head.next);
+        }
+        else{
+            addNode(new Node(key,value));
+            mp.put(key,head.next);
+        }
+
+        
+    }
+}
