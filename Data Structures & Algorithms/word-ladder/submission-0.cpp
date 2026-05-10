b24a39c325d760cd1238654dfa5ee122a86fd292
Rahul Ranjan Jha <84356368+Rahuljha5671@users.noreply.github.com>
2026-05-10 15:50:13 -0700

    Add: word-ladder - submission-0

---
+++ Data Structures & Algorithms/word-ladder/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        int ans=0;
+        queue<pair<string,int>>q;
+
+        unordered_set<string>st(wordList.begin(),wordList.end());
+        st.erase(beginWord);
+        q.push({beginWord,1});
+
+
+        while(!q.empty()){
+            string word=q.front().first;
+            int step=q.front().second;
+            q.pop();
+
+            if(word==endWord){
+                ans=step;
+                break;
+            }
+
+            for(int i=0;i<word.size();i++){
+                char c=word[i];
+                for(char ch='a';ch<='z';ch++){
+                    word[i]=ch;
+                    if(st.find(word)!=st.end()){
+                        q.push({word,step+1});
+                        st.erase(word);
+                    }
+                    word[i]=c;
+                }
+            }  
+        }
+        return ans;
+    }
+};
